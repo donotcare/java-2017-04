b73000f6f405c04fdb9006ff4b83335f52aa3f2f
Жмылев Алексей <bezblagodatnpst@gmail.com>
2017-04-02 22:32:38 +0300

    add obfuscation maven plugin add Test class for obfuscation test

--- src/main/java/ru/otus/lesson1/Main.java
+++ src/main/java/ru/otus/lesson1/Main.java
@@ -2,6 +2,6 @@ package ru.otus.lesson1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Test");
+        System.out.println(Test.name);
     }
 }
---
+++ src/main/java/ru/otus/lesson1/Test.java
@@ -0,0 +1,5 @@
+package ru.otus.lesson1;
+
+public class Test {
+    public final static String name = "Test";
+}
