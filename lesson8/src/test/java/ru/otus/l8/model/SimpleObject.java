94d8cb22143486ea91415148b4d40dac96938035
zhmylev <zhmylevaa@gmail.com>
2017-06-02 11:15:58 +0300

    minor fix

--- lesson8/src/test/java/ru/otus/l8/model/SimpleObject.java
+++ lesson8/src/test/java/ru/otus/l8/model/SimpleObject.java
@@ -10,27 +10,4 @@ public class SimpleObject {
     private Integer testInteger = 100;
     private String name = "Simple object";
     private int testPrimitive = 1;
-
-    public static class JsonSerializerTest {
-        private JsonSerializer serializer;
-        private Gson gson;
-
-        @Before
-        public void setUp() {
-            serializer = new JsonSerializer();
-            gson = new Gson();
-
-        }
-
-        @Test
-        public void serializeSimpleObject() {
-            Assert.assertEquals(gson.toJson(new SimpleObject()), serializer.toJson(new SimpleObject()));
-        }
-
-        @Test
-        public void serializeObjectWithCollections() {
-            Assert.assertEquals(gson.toJson(new ObjectWithCollections()), serializer.toJson(new ObjectWithCollections()));
-        }
-
-    }
 }
